a7880d0d0c1a00978fd84cae90c7425b090f8808
Oszkar <jozsa.oszkar@gmail.com>
2020-07-13 23:13:35 +0900

    Return actual array size and add format.toml
    Bit of error message and formatting cleanup

--- src/board.rs
+++ src/board.rs
@@ -1,4 +1,5 @@
-#[path = "utils.rs"] mod utils; // TODO why do I need to do this? Should I put stuff in subfolders?
+#[path = "utils.rs"] // TODO why do I need to do this? Should I put stuff in subfolders? Am I messing something up?
+mod utils;
 use utils::{gib_loc, gib_num};
 
 pub trait Moves {
@@ -20,8 +21,11 @@ impl Board {
     }
 
     pub fn size(&self) -> u16 {
-        // TODO remove hardcode
-        4
+        // TODO better error handling (it will be more important later when things are dynamic)
+        if self.array.len() != self.array[0].len() {
+            panic!("Array size mismatch!");
+        }
+        self.array.len() as u16
     }
 
     pub fn moves_available(&self) -> bool {
@@ -34,7 +38,9 @@ impl Board {
 
         for j in 0..self.size() {
             for i in 0..self.size() {
-                if self.array[i as usize][j as usize] == 0 { is_zero = true; }
+                if self.array[i as usize][j as usize] == 0 {
+                    is_zero = true;
+                }
             }
         }
 
@@ -44,24 +50,24 @@ impl Board {
 
 impl Default for Board {
     fn default() -> Board {
-        // TODO there might be a better way to store this than u16. We only have 12 possible values
+        // TODO there might be a better way to store this than u16. We only have 12 possible values.
+        // I just thought u16 is small enough and now it is all over the code
         // TODO variable board size
         let mut array = [[0; 4]; 4];
-        
         let (ai, aj) = gib_loc(array);
         let (bi, bj) = loop {
             // need to ensure that the 2 indices are different
             // TODO though actually do we? :) if we don't, we can just use spawn()
             let (tmpi, tmpj) = gib_loc(array);
-            if ai != tmpi || aj != tmpj { break (tmpi, tmpj); }
+            if ai != tmpi || aj != tmpj {
+                break (tmpi, tmpj);
+            }
         };
 
         array[ai as usize][aj as usize] = gib_num();
         array[bi as usize][bj as usize] = gib_num();
 
-        Board {
-            array: array,
-        }
+        Board { array: array }
     }
 }
 
@@ -74,14 +80,17 @@ impl Moves for Board {
             for col in (0..(self.size() - 1)).rev() {
                 let mut zeros_to_the_right = 0;
                 // this loop will go "ahead" and check how many zeros we have to the right (i.e. how many places we need to move the current item)
-                while (col + zeros_to_the_right + 1) < self.size() && self.array[row as usize][(col + zeros_to_the_right + 1) as usize ] == 0 {
+                while (col + zeros_to_the_right + 1) < self.size()
+                    && self.array[row as usize][(col + zeros_to_the_right + 1) as usize] == 0
+                {
                     // we might never hit this part
                     zeros_to_the_right += 1;
                 }
                 // the second clause is there to prevent us from moving zeros (zeros represent empty tile)
-                if zeros_to_the_right > 0 && self.array[row as usize][col as usize ] != 0 {
-                    self.array[row as usize][(col + zeros_to_the_right) as usize ] = self.array[row as usize][col as usize];
-                    self.array[row as usize][col as usize ] = 0;
+                if zeros_to_the_right > 0 && self.array[row as usize][col as usize] != 0 {
+                    self.array[row as usize][(col + zeros_to_the_right) as usize] =
+                        self.array[row as usize][col as usize];
+                    self.array[row as usize][col as usize] = 0;
                     moved = true;
                 }
             }
@@ -97,14 +106,17 @@ impl Moves for Board {
             for col in 1..self.size() {
                 let mut zeros_to_the_left = 0;
                 // this loop will go "ahead" and check how many zeros we have to the left (i.e. how many places we need to move the current item)
-                while (col - zeros_to_the_left) > 0 && self.array[row as usize][(col - zeros_to_the_left - 1) as usize ] == 0 {
+                while (col - zeros_to_the_left) > 0
+                    && self.array[row as usize][(col - zeros_to_the_left - 1) as usize] == 0
+                {
                     // we might never hit this part
                     zeros_to_the_left += 1;
                 }
                 // the second clause is there to prevent us from moving zeros (zeros represent empty tile)
-                if zeros_to_the_left > 0 && self.array[row as usize][col as usize ] != 0 {
-                    self.array[row as usize][(col - zeros_to_the_left) as usize ] = self.array[row as usize][col as usize];
-                    self.array[row as usize][col as usize ] = 0;
+                if zeros_to_the_left > 0 && self.array[row as usize][col as usize] != 0 {
+                    self.array[row as usize][(col - zeros_to_the_left) as usize] =
+                        self.array[row as usize][col as usize];
+                    self.array[row as usize][col as usize] = 0;
                     moved = true;
                 }
             }
@@ -120,14 +132,17 @@ impl Moves for Board {
             for col in 0..self.size() {
                 let mut zeros_to_the_top = 0;
                 // this loop will go "ahead" and check how many zeros we have to the left (i.e. how many places we need to move the current item)
-                while (row - zeros_to_the_top) > 0 && self.array[(row - zeros_to_the_top - 1) as usize][col as usize ] == 0 {
+                while (row - zeros_to_the_top) > 0
+                    && self.array[(row - zeros_to_the_top - 1) as usize][col as usize] == 0
+                {
                     // we might never hit this part
                     zeros_to_the_top += 1;
                 }
                 // the second clause is there to prevent us from moving zeros (zeros represent empty tile)
-                if zeros_to_the_top > 0 && self.array[row as usize][col as usize ] != 0 {
-                    self.array[(row - zeros_to_the_top) as usize][col as usize ] = self.array[row as usize][col as usize];
-                    self.array[row as usize][col as usize ] = 0;
+                if zeros_to_the_top > 0 && self.array[row as usize][col as usize] != 0 {
+                    self.array[(row - zeros_to_the_top) as usize][col as usize] =
+                        self.array[row as usize][col as usize];
+                    self.array[row as usize][col as usize] = 0;
                     moved = true;
                 }
             }
@@ -143,14 +158,17 @@ impl Moves for Board {
             for col in 0..self.size() {
                 let mut zeros_to_the_bottom = 0;
                 // this loop will go "ahead" and check how many zeros we have to the left (i.e. how many places we need to move the current item)
-                while (row + zeros_to_the_bottom + 1) < self.size() && self.array[(row + zeros_to_the_bottom + 1) as usize][col as usize ] == 0 {
+                while (row + zeros_to_the_bottom + 1) < self.size()
+                    && self.array[(row + zeros_to_the_bottom + 1) as usize][col as usize] == 0
+                {
                     // we might never hit this part
                     zeros_to_the_bottom += 1;
                 }
                 // the second clause is there to prevent us from moving zeros (zeros represent empty tile)
-                if zeros_to_the_bottom > 0 && self.array[row as usize][col as usize ] != 0 {
-                    self.array[(row + zeros_to_the_bottom) as usize][col as usize ] = self.array[row as usize][col as usize];
-                    self.array[row as usize][col as usize ] = 0;
+                if zeros_to_the_bottom > 0 && self.array[row as usize][col as usize] != 0 {
+                    self.array[(row + zeros_to_the_bottom) as usize][col as usize] =
+                        self.array[row as usize][col as usize];
+                    self.array[row as usize][col as usize] = 0;
                     moved = true;
                 }
             }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
+use crossterm::{
+    cursor,
+    event::{read, Event, KeyCode},
+    style::Colorize,
+    terminal::{Clear, ClearType},
+    ExecutableCommand,
+};
 use std::{io::stdout, process};
-use crossterm::{ExecutableCommand, cursor, terminal::{Clear, ClearType}, event::{read, Event, KeyCode}, style::{Colorize}};
+// TODO I might want to play with the log crate
 
 mod board;
 use board::{Board, Moves};
@@ -15,10 +22,18 @@ enum EndState {
 // TODO this probably should be in a Display trait
 fn print_matrix(arr: &[[u16; 4]; 4]) {
     let mut stdout = stdout();
-    stdout.execute(Clear(ClearType::All)).expect("UH OH");
-    stdout.execute(cursor::MoveTo(0, 0)).expect("UH OH");
-    
-    println!("{}", "Welcome to the Matrix. You know the drill. Just use the arrows".blue());
+    // TODO should we panic here just because screen operation failed? It is quite unexpected...
+    stdout
+        .execute(Clear(ClearType::All))
+        .expect("Terminal screen clearing failed.");
+    stdout
+        .execute(cursor::MoveTo(0, 0))
+        .expect("Terminal cursor updat failed.");
+
+    println!(
+        "{}",
+        "Welcome to the Matrix. You know the drill. Just use the arrows".blue()
+    );
     println!();
 
     for (_i, row) in arr.iter().enumerate() {
@@ -35,42 +50,57 @@ fn print_matrix(arr: &[[u16; 4]; 4]) {
 }
 
 fn tear_down(done: EndState) {
+    println!();
     // TODO better messages
     match done {
         EndState::UserQuit => {
             println!("Kabbe gyikok");
             process::exit(0);
-        },
+        }
         EndState::Win => {
             println!("GG");
             process::exit(0);
-        },
+        }
         EndState::Lose => {
             println!("{}", ":(".dark_red());
             process::exit(0);
-        },
+        }
         EndState::Error => {
             println!("Ooh la la, something went wrong");
             process::exit(0);
-        },
+        }
     }
-    
 }
 
 fn main() {
     let mut b = Board::default();
     print_matrix(&b.array);
-    
     loop {
         // `read()` blocks until an `Event` is available
-        match read().expect("OH NOES") {
+        match read().expect("Something went very wrong with the keyboard input.") {
             Event::Key(event) => {
                 match event.code {
                     // TODO move this logic into Board
-                    KeyCode::Up => if b.move_up() { b.spawn() },
-                    KeyCode::Down => if b.move_down() { b.spawn() },
-                    KeyCode::Left => if b.move_left() { b.spawn() },
-                    KeyCode::Right => if b.move_right() { b.spawn() },
+                    KeyCode::Up => {
+                        if b.move_up() {
+                            b.spawn()
+                        }
+                    }
+                    KeyCode::Down => {
+                        if b.move_down() {
+                            b.spawn()
+                        }
+                    }
+                    KeyCode::Left => {
+                        if b.move_left() {
+                            b.spawn()
+                        }
+                    }
+                    KeyCode::Right => {
+                        if b.move_right() {
+                            b.spawn()
+                        }
+                    }
                     KeyCode::Esc => tear_down(EndState::UserQuit),
                     _ => (),
                 }
@@ -78,6 +108,8 @@ fn main() {
             _ => (), // we don't care about mouse and resize events for now
         }
         print_matrix(&b.array);
-        if !b.moves_available() { tear_down(EndState::Lose); }
+        if !b.moves_available() {
+            tear_down(EndState::Lose);
+        }
     }
 }
