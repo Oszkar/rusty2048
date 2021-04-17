920b5d50c8dbc6eeb17f31083e0d3c06a9a3fdbd
Oszkar <jozsa.oszkar@gmail.com>
2021-04-17 16:00:17 +0900

    Bump dependency versions

--- src/board/utils.rs
+++ src/board/utils.rs
@@ -23,8 +23,8 @@ pub fn gib_empty_loc(arr: [[u16; 4]; 4]) -> (u16, u16) {
 
     // TODO what if there is no empty?
     let idx = loop {
-        let i = rng.gen_range(0, 4);
-        let j = rng.gen_range(0, 4);
+        let i = rng.gen_range(0..4);
+        let j = rng.gen_range(0..4);
         // need to ensure that we are trying to spawn on an empty location
         if arr[i as usize][j as usize] == 0 {
             break (i, j);
